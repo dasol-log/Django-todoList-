0a7bcd5a713046e0e721ed73e3e991208f308651
Dasol Yoon <dasol0429@gmail.com>
2026-03-05 18:11:52 +0900

    chore:.env파일 추가 및 settings 변경

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ  # 환경변수 추가
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#n5+fct=45$29p3@&#yxq%9cm-x6n-1)qbz=n1m$j@42x!iq#*"
+# 보안 향상, 코드 재사용, 환경 구분 가능
+env = environ.Env(DEBUG=(bool, False))
+
+# 환경변수
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# SECURITY를 .env로 이동하여 보호
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
